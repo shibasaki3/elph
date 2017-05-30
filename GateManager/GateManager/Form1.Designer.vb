66e083357e945a994855664fdc03e57e8fa5425c
shibasaki3 <shibasak@lns.tohoku.ac.jp>
2017-05-30 11:06:38 +0900

    Gate Manager
    Barcode type

---
+++ GateManager/GateManager/Form1.Designer.vb
@@ -0,0 +1,199 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.HomeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.AxEng1 = New AxFAENGINELib5.AxEng()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.AxEng1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 27)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(250, 29)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(269, 27)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(172, 19)
+        Me.TextBox2.TabIndex = 1
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.HomeToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1199, 26)
+        Me.MenuStrip1.TabIndex = 2
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'HomeToolStripMenuItem
+        '
+        Me.HomeToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
+        Me.HomeToolStripMenuItem.Name = "HomeToolStripMenuItem"
+        Me.HomeToolStripMenuItem.Size = New System.Drawing.Size(55, 22)
+        Me.HomeToolStripMenuItem.Text = "Home"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(98, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MS UI Gothic", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(12, 59)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(883, 64)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "さんは入室を許可されていません。"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("MS UI Gothic", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label2.Location = New System.Drawing.Point(23, 388)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(22, 33)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "l"
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 10000
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(29, 437)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 19)
+        Me.TextBox3.TabIndex = 5
+        Me.TextBox3.Text = "9"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(145, 437)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(107, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "SetMyRoomID"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'AxEng1
+        '
+        Me.AxEng1.Enabled = True
+        Me.AxEng1.Location = New System.Drawing.Point(502, 429)
+        Me.AxEng1.Name = "AxEng1"
+        Me.AxEng1.OcxState = CType(resources.GetObject("AxEng1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxEng1.Size = New System.Drawing.Size(36, 36)
+        Me.AxEng1.TabIndex = 7
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("MS UI Gothic", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 123)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(304, 97)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("MS UI Gothic", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label4.Location = New System.Drawing.Point(18, 220)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(304, 97)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Label4"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1199, 463)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.AxEng1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.AxEng1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents HomeToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents AxEng1 As AxFAENGINELib5.AxEng
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ GateManager/GateManager/Form1.vb
@@ -0,0 +1,217 @@
+﻿Public Class Form1
+    Private ws As WebReference.KanriService
+    Private destR As WebReference.RoomsInfo
+    'Private wss As WebReference.PersonInfo
+    Private jpnPurpose() As String
+    Private engPurpose() As String
+
+    Private idCol As Collection
+    Private MyRoomID As Integer
+
+    Private FLock As Boolean
+
+    Private EnterdPersons As Collection '入室済みの人のIDを保存
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Me.FormBorderStyle = Windows.Forms.FormBorderStyle.Fixed3D
+
+        idCol = New Collection
+        EnterdPersons = New Collection
+
+        ReadMyRooms()
+
+        ws = New WebReference.KanriService
+
+        'PLCと接続
+        Me.AxEng1.StartUpdate()
+
+        Me.Timer1.Enabled = True
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Dim tmpBx As TextBox = CType(sender, TextBox)
+        Dim a As String = tmpBx.Text
+        Dim pno As String
+        Dim sno As String
+        If InStr(a, vbCrLf) Then
+            '読み取り器の番号を判定
+            Dim b As String = Microsoft.VisualBasic.Left(a, 1)
+            If b = "B" Then
+                sno = "2"
+            Else
+                sno = "1"
+            End If
+            'フィルムバッチかどうかを判定
+            Dim l As String = Microsoft.VisualBasic.Mid(a, 2, 1)
+            If l Like "[0-9]" Then
+                pno = "0" & Microsoft.VisualBasic.Mid(a, 2, 5)
+            Else
+                Dim j As Integer = Asc(l) - &H40
+
+                pno = Format(j, "0") & Microsoft.VisualBasic.Mid(a, 3, 5)
+            End If
+
+            'Me.MessageQueue1.Send(sno & pno)
+            Me.TextBox2.Text = sno & pno
+            idCol.Add(sno & pno)
+            tmpBx.Text = ""
+
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim f As Form = Form.ActiveForm
+        If f Is Nothing Then
+            'Me.TextBox2.Text = "Not Active!!"
+            Me.WindowState = FormWindowState.Minimized
+            Me.WindowState = FormWindowState.Normal
+            Me.TextBox1.Focus()
+            'Else
+            'Me.TextBox2.Text = "Now Active!!"
+        End If
+        EntryPerson()
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+        Me.Dispose()
+    End Sub
+
+    Private Sub EntryPerson()
+        If idCol.Count = 0 Then Exit Sub
+        Dim strCardID As String = idCol(1)
+        idCol.Remove(1)
+        Dim CardID As Integer = Val(Microsoft.VisualBasic.Right(strCardID, 6))
+        Dim direction As Integer = Val(Microsoft.VisualBasic.Left(strCardID, 1))
+        Me.Label1.Text = ""
+        Dim wss1 As WebReference.PersonInfo = ws.GetPersonInfo(CardID)
+        If wss1.PersonName = "" Then
+            Me.Label1.Text = "ID(" & CardID.ToString & ")が見つかりません"
+            Exit Sub
+        End If
+        '入室か退室かをチェック
+        If direction = 1 Then '入室の場合
+             '既に入室状態となっているかどうかをチェック
+            If ws.ExistRoom(CardID, MyRoomID) Then
+                Dim msg As String
+                If wss1.LangType = "Jpn" Then
+                    msg = wss1.PersonName & "さんは既に入室状態です。"
+                Else
+                    msg = wss1.PersonName & " is already exist."
+                End If
+                Me.Label1.Text = msg
+
+                '入室が可能かどうかをチェックする
+            ElseIf Not ws.IsThisAllow(CardID, MyRoomID) Then
+                Dim msg As String
+                If wss1.LangType = "Jpn" Then
+                    msg = wss1.PersonName & "さんは入室を許可されていません。"
+                Else
+                    msg = wss1.PersonName & " is not allowed to enter this room."
+                End If
+                Me.Label1.Text = msg
+                Exit Sub
+            Else
+                ReDim wss1.DestRooms(0)
+                wss1.DestRooms(0) = MyRoomID
+                'wss1.Purpose = "作業"
+
+                Dim a As String = ws.RegistEnter(wss1)
+                Dim b As String = ws.GateEnter(wss1, MyRoomID)
+                EnterdPersons.Add(wss1.CardID, wss1.CardID.ToString)
+                TurnOnOffEmptyLamp(False)
+            End If
+
+        Else '退室の場合
+            Dim j As Integer = ws.CountEnteredPerson(MyRoomID)
+            Dim rtval As String = ws.RegistExit(wss1, MyRoomID)
+            EnterdPersons.Remove(wss1.CardID.ToString)
+            If EnterdPersons.Count = 0 Then
+                TurnOnOffEmptyLamp(True)
+            End If
+        End If
+        '扉を解錠するコード
+        Me.DoorUnLock()
+        Me.Label2.Text = "解錠中"
+        Me.Timer2.Enabled = True
+        '
+    End Sub
+
+    Private Sub ReadMyRooms()
+        MyRoomID = My.Computer.FileSystem.ReadAllText("MyRoomID.txt")
+    End Sub
+
+    '解錠後に指定時間内に扉が開かなかった時は施錠する
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Me.DoorLock()
+        'Me.Label2.Text = ""
+        Me.Timer2.Enabled = False
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MyRoomID = Me.TextBox3.Text
+    End Sub
+
+    Private Sub AxEng1_ValueChanged(sender As Object, e As AxFAENGINELib5._DFAEngineEvents_ValueChangedEvent) Handles AxEng1.ValueChanged
+        Dim tagname() As String = e.tagPath.Split(".")
+        If tagname.Length <> 3 Then Exit Sub
+        Select Case tagname(2)
+            Case "UNLOCK"
+                If e.value Then
+                    FLock = False
+                    Me.Label3.Text = "解錠中"
+                End If
+            Case "LOCK"
+                If e.value Then
+                    FLock = True
+                    Me.Label3.Text = "施錠中"
+                End If
+            Case "CLOSE"
+                If e.value Then
+                    '扉が閉じた直後に施錠する
+                    Me.DoorLock()
+                    Me.Label4.Text = "扉クローズ"
+                Else
+                    '扉が開けられたら施錠タイマーを解除する
+                    Me.Timer2.Enabled = False
+                    Me.Label4.Text = "扉オープン"
+                End If
+            Case "CONFIRM"
+                If EnterdPersons.Count > 0 Then
+                    ExecRoomEmpty()
+                End If
+        End Select
+    End Sub
+
+
+
+    Private Sub DoorLock()
+        If Not FLock Then
+            Me.AxEng1.WriteVal("U03.F01.OLOCK", True)
+            Me.Label2.Text = ""
+            Me.Label1.Text = ""
+        End If
+    End Sub
+
+    Private Sub DoorUnLock()
+        If FLock Then
+            Me.AxEng1.WriteVal("U03.F01.OUNLOCK", True)
+        End If
+    End Sub
+
+    Private Sub ExecRoomEmpty()
+        Do
+            If EnterdPersons.Count = 0 Then Exit Do
+            Dim CardID = EnterdPersons.Item(1)
+
+            Dim wss1 As WebReference.PersonInfo = ws.GetPersonInfo(CardID)
+            Dim rtval As String = ws.RegistExit(wss1, MyRoomID)
+
+            EnterdPersons.Remove(CardID.ToString)
+        Loop
+    End Sub
+
+    Private Sub TurnOnOffEmptyLamp(ByVal a As Boolean)
+        Me.AxEng1.WriteVal("U03.F01.EMPTY", a)
+    End Sub
+End Class
